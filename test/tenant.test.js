c6db17384e16fa7e92e4041b31bfe79fd7a852b8
Muhammad Taufiq Hidayah <hidayahhtaufik12@gmail.com>
2021-05-29 14:14:57 +0700

    fixxing tdd

--- test/tenant.test.js
+++ test/tenant.test.js
@@ -69,7 +69,7 @@ describe("test tenant's CRUD section", () => {
           .post('/tenant')
           .set('access_token', `${access_token}`)
           .send({
-            email: 'pengunjung0@customer.com',
+            email: 'pengunjun9@customer.com',
             name: 'customer',
             phone: '0812382620',
             checkIn: "2021-03-12",
