27f0fc5fc261312a884ba2cf0fa1c3a9372ff671
billybep <billy.pesoth@gmail.com>
2021-05-29 09:57:56 +0700

    add routing and controller CRUD functionality

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const {authentication} = require("../middlewares/auth.js");
 const userRouter = require("./user");
 const propertyRouter = require('./property')
 const tenantRouter = require("./tenant")
+const revenueRouter = require('./revenue')
 
 
 router.use("/", userRouter)
 
 router.use(authentication)
 router.use('/properties', propertyRouter)
+router.use('/revenues', revenueRouter)
 
 router.use('/tenant', tenantRouter);
 
