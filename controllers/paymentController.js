324e0c176445651fd19528b06c7bf3d06207f176
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-06-01 00:25:11 +0700

    test ihsan

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,114 +1,117 @@
-const {Room, Tenant, Payment} = require("../models")
+const { Room, Tenant, Payment } = require('../models');
 class PaymentController {
-    static createPayment(req, res, next) {
-        let {month, year, nextDueDate, paidCash} = req.body;
-        let {roomId, tenantId} = req.params;
-        
-        Payment.create({
-            month,
-            year,
-            nextDueDate,
-            paidCash,
-            roomId,
-            tenantId
-        })
-        .then(data => {
-            res.status(201).json(data);
-        })
-        .catch(err => {
-            console.log(err);
-            next(err);
-        })
-    }
+  static createPayment(req, res, next) {
+    let { month, year, nextDueDate, paidCash } = req.body;
+    let { roomId, tenantId } = req.params;
 
-    static findPayments(req, res, next) {
-        Payment.findAll({
-            include: [
-                {
-                    model: Room
-                },
-                {
-                    model: Tenant
-                }
-            ]
-        })
-        .then(data => {
-            res.status(200).json(data);
-        })
-        .catch(err => {
-            next(err);
-        })
-    }
+    Payment.create({
+      month,
+      year,
+      nextDueDate,
+      paidCash,
+      roomId,
+      tenantId,
+    })
+      .then((data) => {
+        res.status(201).json(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        next(err);
+      });
+  }
 
-    static deletePayment(req, res, next) {
-        let id = req.params.id;
-        Payment.destroy({
-            where: {
-                id
-            }
-        })
-        .then(data => {
+  static findPayments(req, res, next) {
+    Payment.findAll({
+      include: [
+        {
+          model: Room,
+        },
+        {
+          model: Tenant,
+        },
+      ],
+    })
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 
-            if(data === 0) {
-                next({name: "PaymentNotFound"});
-            } else {
-                res.status(200).json({
-                    msg: "Payment successfully deleted"
-                })
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            next(err);  
-        })
-    }
+  static deletePayment(req, res, next) {
+    let id = req.params.id;
+    Payment.destroy({
+      where: {
+        id,
+      },
+    })
+      .then((data) => {
+        if (data === 0) {
+          next({ name: 'PaymentNotFound' });
+        } else {
+          res.status(200).json({
+            msg: 'Payment successfully deleted',
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        next(err);
+      });
+  }
 
-    static findPaymentById(req, res, next) {
-        let id = req.params.id;
-        Payment.findByPk(id)
-            .then(data => {
-                console.log(data, "LAGI DI PAYMENT FIND BY PK")
-                if(data === null) {
-                    next({name: "PaymentNotFound"})
-                } else {
-                    res.status(200).json(data);
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                next(err);
-            })
-    }
+  static findPaymentById(req, res, next) {
+    let id = req.params.id;
+    Payment.findByPk(id)
+      .then((data) => {
+        console.log(data, 'LAGI DI PAYMENT FIND BY PK');
+        if (data === null) {
+          next({ name: 'PaymentNotFound' });
+        } else {
+          res.status(200).json(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        next(err);
+      });
+  }
 
-    static editPayment(req, res, next) {
-        let {id} = req.params;
-        let {month, year, nextDueDate, paidCash} = req.body;
+  static editPayment(req, res, next) {
+    let { id } = req.params;
+    let { month, year, nextDueDate, paidCash } = req.body;
 
-        Payment.update({
-            month,
-            year,
-            nextDueDate,
-            paidCash,
-        }, {
-            where: {
-                id
-            },
-            returning: true
-        })
-        .then(data => {
-            if(data[0] === 0) {
-                next({name: "PaymentNotFound"})
-            } else {
-                res.status(200).json({
-                    msg: "Payment updated successfully",
-                    updatedData: data[1][0]
-                })
-            }
-        })
-        .catch(err => {
-            next(err);
-        })
-    }
+    Payment.update(
+      {
+        month,
+        year,
+        nextDueDate,
+        paidCash,
+      },
+      {
+        where: {
+          id,
+        },
+        returning: true,
+      }
+    )
+      .then((data) => {
+        if (data[0] === 0) {
+          console.log(data);
+          next({ name: 'PaymentNotFound' });
+        } else {
+          res.status(200).json({
+            msg: 'Payment updated successfully',
+            updatedData: data[1][0],
+          });
+        }
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 }
 
 module.exports = PaymentController;
