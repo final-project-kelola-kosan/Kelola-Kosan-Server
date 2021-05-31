175b9f9996d2ed83a3c76b1e612db998f3a9ec7d
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-05-31 14:59:43 +0700

    Testing for client

--- seeders/20210528192056-add-tenants.js
+++ seeders/20210528192056-add-tenants.js
@@ -2,28 +2,32 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Tenants", [
-      {
-        id: 1,
-        email: "joko@mail.com",
-        name: "joko",
-        phone: "0823452",
-        checkIn: new Date(),
-        checkOut: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        id: 2,
-        email: "papang@mail.com",
-        name: "papang",
-        phone: "0883249821",
-        checkIn: new Date(),
-        checkOut: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ], {})
+    return queryInterface.bulkInsert(
+      'Tenants',
+      [
+        {
+          id: 3,
+          email: 'ihsan@mail.com',
+          name: 'ihsan',
+          phone: '0823452234234',
+          checkIn: new Date(),
+          checkOut: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 4,
+          email: 'abdul@mail.com',
+          name: 'abdul',
+          phone: '08832498213242',
+          checkIn: new Date(),
+          checkOut: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
     /**
      * Add seed commands here.
      *
@@ -32,16 +36,16 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("Tenants", null, {})
+    return queryInterface.bulkDelete('Tenants', null, {});
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
