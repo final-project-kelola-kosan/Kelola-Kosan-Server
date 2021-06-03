ee58705b7def69bcdcf6bdd0f7e3057d349b320d
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-06-03 16:07:43 +0700

    before demo

--- helpers/cron.js
+++ helpers/cron.js
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
 const cron = require('node-cron');
 const moment = require('moment');
-const { sendMail } = require('../controllers/mvp/nodemailer');
-
-const paymentRemainder = (paymentDate, tenant) => {
-  console.log(paymentDate, ' standarrt');
-  let notyTime = moment(paymentDate);
-  console.log(notyTime, 'tanggal pembayaran<<!');
-=======
-const cron = require('node-cron')
-const moment = require('moment')
-const {sendMailTenant} = require("../controllers/mvp/nodemailer");
-const fs = require("fs");
+const { sendMailTenant } = require('../controllers/mvp/nodemailer');
+const fs = require('fs');
 
 //paymentRemainder(duedate, data.email, data.name, userData, roomData)
-const paymentRemainder = (paymentDate, tenantEmail, tenantName, userData, roomData) => {
->>>>>>> 4f07a6a4e63fd0c2310a6f8e49d807ab586a8bfc
-
+const paymentRemainder = (
+  paymentDate,
+  tenantEmail,
+  tenantName,
+  userData,
+  roomData
+) => {
   // notyTime
   //   .add(2, 'months')
   //   .subtract(3, 'days')
@@ -27,31 +21,24 @@ const paymentRemainder = (paymentDate, tenantEmail, tenantName, userData, roomDa
 
   // console.log(getMonth, getDay, 'month to create schedule crone');
 
-<<<<<<< HEAD
-  let generateSchedule = `51 19 ${getDay} ${getMonth} *`;
+  let generateSchedule = `0 9 ${getDay} ${getMonth} *`;
   console.log(
     generateSchedule,
     'variable masuk ke cron !!!!!!!!!!!!!!!!!!!!!!!!'
   );
-
-=======
-  let generateSchedule = `0 9 ${getDay} ${getMonth} *`
-  console.log(generateSchedule, 'variable masuk ke cron !!!!!!!!!!!!!!!!!!!!!!!!');
->>>>>>> 4f07a6a4e63fd0c2310a6f8e49d807ab586a8bfc
   cron.schedule(generateSchedule, () => {
     console.log('Kirim EMAIL Remainder Pembayaran Kos.....');
     //? codingan email disini
-<<<<<<< HEAD
+    sendMailTenant(
+      `Payment reminder`,
+      'Information',
+      tenantEmail,
+      tenantName,
+      userData,
+      roomData
+    );
   });
-
-  // cron.schedule('* * * * * *', () => {
-  //   // console.log('CRON SCHEDULER JALAN');
-  // });
-=======
-    sendMailTenant(`Payment reminder`, "Information", tenantEmail, tenantName, userData, roomData);
-  })
   // sendMailTenant(`Payment reminder`, "Information", tenantEmail, tenantName, userData, roomData);
->>>>>>> 4f07a6a4e63fd0c2310a6f8e49d807ab586a8bfc
 
   return `${notyTime} <<<`;
 };
--- seeders/20210528192056-add-tenants.js
+++ seeders/20210528192056-add-tenants.js
@@ -1,57 +1,61 @@
 'use strict';
 
-const dateString = '2021-04-01'
-var firstDate = new Date(dateString + "T00:00:00");
+const dateString = '2021-04-01';
+var firstDate = new Date(dateString + 'T00:00:00');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Tenants", [
-      {
-        email: "qojack82nasution@gmail.com",
-        name: "Jack",
-        phone: "0823452",
-        checkIn: firstDate,
-        checkOut: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: "jecksfresley@gmail.com",
-        name: "Jecksen",
-        phone: "085398464583",
-        checkIn: firstDate,
-        checkOut: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: "hidayatarifin063@gmail.com",
-        name: "Arifin",
-        phone: "081347767810",
-        checkIn: twotDate,
-        checkOut: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: "hidayahtaufikk12@gmail.com",
-        name: "Taufik",
-        phone: "089693224053",
-        checkIn: threetDate,
-        checkOut: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: "HarunsAS@gmail.com",
-        name: "Harun",
-        phone: "085391818787",
-        checkIn: fourDate,
-        checkOut: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ], {})
+    return queryInterface.bulkInsert(
+      'Tenants',
+      [
+        {
+          email: 'qojack82nasution@gmail.com',
+          name: 'Jack',
+          phone: '0823452',
+          checkIn: firstDate,
+          checkOut: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          email: 'jecksfresley@gmail.com',
+          name: 'Jecksen',
+          phone: '085398464583',
+          checkIn: firstDate,
+          checkOut: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          email: 'hidayatarifin063@gmail.com',
+          name: 'Arifin',
+          phone: '081347767810',
+          checkIn: firstDate,
+          checkOut: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          email: 'hidayahtaufikk12@gmail.com',
+          name: 'Taufik',
+          phone: '089693224053',
+          checkIn: firstDate,
+          checkOut: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          email: 'HarunsAS@gmail.com',
+          name: 'Harun',
+          phone: '085391818787',
+          checkIn: firstDate,
+          checkOut: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
     /**
      * Add seed commands here.
      *
@@ -60,16 +64,16 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("Tenants", null, {})
+    return queryInterface.bulkDelete('Tenants', null, {});
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
