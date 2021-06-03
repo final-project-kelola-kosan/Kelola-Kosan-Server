b83d7872196958be3138f00a3693a53c087f739f
Muhammad Taufiq Hidayah <68336187+hidayahhtaufik@users.noreply.github.com>
2021-06-03 15:09:51 +0700

    Update 20210528132729-add-property.js

--- seeders/20210528132729-add-property.js
+++ seeders/20210528132729-add-property.js
@@ -13,10 +13,10 @@ module.exports = {
     .then(data => {
       return queryInterface.bulkInsert("Properties", [
         {
-          name: "Wisma",
-          address: "Jaksel",
-          image: "https://images.unsplash.com/photo-1597047084897-51e81819a499?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80",
-          phone: "14045",
+          name: "Wisma Andara",
+          address: "Jl. Andara Raya, Pangkalan Jati Baru, Kec. Cinere, Kota Depok, Jawa Barat.",
+          image: "https://blogpictures.99.co/sumber-gridid2.png",
+          phone: "082110112795",
           userId: data.id,
           createdAt: new Date(),
           updatedAt: new Date()
